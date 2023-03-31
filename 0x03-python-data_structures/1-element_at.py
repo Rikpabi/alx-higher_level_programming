ee7b3dee175c1e5446a3b07e712e6ebbd4bf68db
Richard Akpabio <richardakpabio0@gmail.com>
2023-03-31 13:09:26 -0700

    Element at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    if (my_list is None) or (idx is None)\
+       or (idx < 0) or (idx >= len(my_list)):
+        return
+    return my_list[idx]
