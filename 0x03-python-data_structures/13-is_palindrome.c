d78f442bd7b6bc3f7c41a72c219f8cf7c2fdc56b
Richard Akpabio <richardakpabio0@gmail.com>
2023-03-31 13:53:04 -0700

    Is palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,35 +1,37 @@
 #include "lists.h"
-/**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: linked list.
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
+
+listint_t *is_pal_recurs(listint_t *left, listint_t *right)
+{
+	if (right->next == NULL)
+		return (left->next);
+	right = right->next;
+	if (right->next == NULL)
+	{
+		if (left->n != left->next->n)
+			return (NULL);
+		if (left->next->next == NULL)
+			return (left);
+		return (left->next->next);
+	}
+	right = is_pal_recurs(left->next, right->next);
+	if (right == NULL)
+		return (NULL);
+	if (right->n == left->n)
+	{
+		if (right->next == NULL)
+			return (left);
+		return (right->next);
+	}
+	return (NULL);
+}
+		
+
 int is_palindrome(listint_t **head)
 {
-	listint_t *p = NULL;
-	int i = 0, size = 0, j = 0, flag = 1;
-	int *a = NULL;
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+		return (1);
 
-	if (head == NULL || *head == NULL)
-		return (flag);
-	p = *head;
-	while (p->next != NULL)
-	{
-		size++;
-		p = p->next;
-	}
-	a = malloc(sizeof(int) * size + 1);
-	p = *head;
-	while (p != NULL)
-	{
-		a[i] = p->n;
-		i++;
-		p = p->next;
-	}
-	j = size;
-	for (i = 0; i <= size / 2; i++, j--)
-		if (a[i] != a[j])
-			flag = 0;
-	free(a);
-	return (flag);
+	if (is_pal_recurs(*head, *head) != NULL)
+		return (1);
+	return (0);
 }
