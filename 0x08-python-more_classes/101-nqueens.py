7b43d32a57bcc825d6504e583fe6750dd0cfed52
Richard Akpabio <richardakpabio0@gmail.com>
2023-04-03 09:19:30 -0700

    Nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,54 +1,62 @@
 #!/usr/bin/python3
-"""Standalone module to solve the nqueens problem"""
+"""
+N queens backtracking program to print the coordinates of N queens
+on an NxN grid such that they are all in non-attacking positions
+"""
 
 
-import sys
+from sys import argv
 
-
-def nqueens(size):
-    """Initial setup before recursive call"""
-    if type(size) is not int:
+if __name__ == "__main__":
+    a = []
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if argv[1].isdigit() is False:
         print("N must be a number")
-        return
-    if size < 4:
+        exit(1)
+    n = int(argv[1])
+    if n < 4:
         print("N must be at least 4")
-        return
-    queens = [0] * size
+        exit(1)
 
-    def printsolution(queens):
-        print("[[0, ", queens[0], "]", sep="", end="")
-        for y, x in enumerate(queens[1:], 1):
-            print(", [", y, ", ", x, "]", sep="", end="")
-        print("]")
+    # initialize the answer list
+    for i in range(n):
+        a.append([i, None])
 
-    def queencalc(queen):
-        """Recursive call queen position validator"""
-        for x in range(size):
-            """horizontal board positions per queen"""
-            nextx = 0
-            for y in range(queen):
-                qx = queens[y]
-                if x == qx or x + queen == qx + y or x - queen == qx - y:
-                    nextx = 1
-                    break
-            if nextx == 1:
-                nextx == 0
-                continue
-            if queen != size - 1:
-                queens[queen + 1] = 0
-                queens[queen] = x
-                queencalc(queen + 1)
-            else:
-                queens[queen] = x
-                printsolution(queens)
-    queencalc(0)
+    def already_exists(y):
+        """check that a queen does not already exist in that y value"""
+        for x in range(n):
+            if y == a[x][1]:
+                return True
+        return False
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit()
-try:
-    size = int(sys.argv[1])
-except (ValueError, TypeError):
-    print("N must be a number")
-    exit()
-nqueens(size)
+    def reject(x, y):
+        """determines whether or not to reject the solution"""
+        if (already_exists(y)):
+            return False
+        i = 0
+        while(i < x):
+            if abs(a[i][1] - y) == abs(i - x):
+                return False
+            i += 1
+        return True
+
+    def clear_a(x):
+        """clears the answers from the point of failure on"""
+        for i in range(x, n):
+            a[i][1] = None
+
+    def nqueens(x):
+        """recursive backtracking function to find the solution"""
+        for y in range(n):
+            clear_a(x)
+            if reject(x, y):
+                a[x][1] = y
+                if (x == n - 1):  # accepts the solution
+                    print(a)
+                else:
+                    nqueens(x + 1)  # moves on to next x value to continue
+
+    # start the recursive process at x = 0
+    nqueens(0)
